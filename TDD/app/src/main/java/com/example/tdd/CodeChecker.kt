8011afe27dbb2aaabdbe2328aee7cd04893fa877
Min-Jae-Bae <qoalswo123@icloud.com>
2022-10-13 14:39:15 +0900

    세번째 테스트 [성공]: 숫자를 포함하지 않고 나머지 충족하는 테스트 및 중복 제거 (메소드 추출)

--- TDD/app/src/main/java/com/example/tdd/CodeChecker.kt
+++ TDD/app/src/main/java/com/example/tdd/CodeChecker.kt
@@ -1,5 +1,7 @@
 package com.example.tdd
 
+
+
 /*
 암호 검사기 규칙에 따라 암호를 강함, 보통, 약함
 
@@ -11,8 +13,23 @@ package com.example.tdd
 세 규칙을 모두 충족하면 암호는 강함
 2개의 규칙을 충족하면 암호는 보통
 1개 이하의 규칙을 충족하면 암호는 약함
-*/
 
+숫자를 포함하지 않는 경우 NORMAL 값 리턴
+문자열 하나하나를 선택하여 확인 숫자가 있으면 true 반환
+-> 기본 값 포함되어 있지 않음(false), 변수 ContainNumber, 문자열 변수 char 문자열을 하나씩 확인하기 위해서 for 사용, 문자열을
+확인하는 범위 값, 각 문자열에 대해 포함되어 있으면 true 그렇지 않으면 false 반환
+숫자가 포함되어 있으면 STRONG 반환, 포함되지 않았으면 NORMAL 반환
+
+숫자 포함 확인 containNumber
+문자열 안에 숫자 (0~9)가 있는지 인덱스마다 확인 ( for )
+
+숫자를 포함 했을 때  ( True )
+- STRONG 반환
+- break
+
+숫자를 포함 하지 않았을 때 ( False )
+-NORMAL 반환
+*/
 
 enum class PasswordStrength {
     STRONG,
@@ -26,25 +43,24 @@ class PasswordStrengthMeter {
         if (s.length < 8) {
             return PasswordStrength.NORMAL
         }
-        /*
-        숫자를 포함하지 않는 경우 NORMAL 값 리턴
-        문자열 하나하나를 선택하여 확인 숫자가 있으면 true 반환
-        -> 기본 값 포함되어 있지 않음(false), 변수 ContainNumber, 문자열 변수 char 문자열을 하나씩 확인하기 위해서 for 사용, 문자열을
-        확인하는 범위 값, 각 문자열에 대해 포함되어 있으면 true 그렇지 않으면 false 반환
-        숫자가 포함되어 있으면 STRONG 반환, 포함되지 않았으면 NORMAL 반환
-        */
-        var containNumber: Boolean = false
-        for (i in s.indices) {
-            val char = s.elementAt(i)
-            if (char.code < 48 || char.code > 57) {
-                containNumber = true
-                break
-            }
-        }
+
+        // 숫자를 받아 포함되어 있는지 true, false 저장
+        val containNumber = meetSContainingNumberCriteria(s)
         if (!containNumber) return PasswordStrength.NORMAL
 
         return PasswordStrength.STRONG
     }
+    // 문자를 받아 숫자가 포함되어 있는지 확인해주는 기능
+    private fun meetSContainingNumberCriteria(s: String): Boolean {
+        for (i in s.indices) {
+            val char = s[i]
+            if (char in '0'..'9') {
+                return true
+            }
+        }
+        return false
+    }
+
 
 }
 
--- TDD/app/src/test/java/com/example/tdd/CodeCheckerTest.kt
+++ TDD/app/src/test/java/com/example/tdd/CodeCheckerTest.kt
@@ -4,34 +4,40 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.jupiter.api.DisplayName
 
+/*
+ 생성자와, 비밀번호, 측정이 중복되었다.
+ 하나의 기능으로 뽑았다.
+
+ */
+
 class CodeCheckerTest {
 
     class PasswordStrengthTest {
 
+
         @DisplayName("모든 규칙을 충족하는 테스트")
         @Test
         fun `When meet all criteria expect strong`() {
-            val meter = PasswordStrengthMeter() // 암호 강함 측정 클래스 생성하고 meter 변수에 넣음
-            val result: PasswordStrength =
-                meter.meter("aA123!58") // 결과 변수 타입은 암호 강함  = 그것은 측정 클래스의 측정 기능으로 "aA123!58"를 측정한 것
-            assertEquals(PasswordStrength.STRONG, result) // 강력한 암호라는 것을 증명함
+            assertStrength("aA123!58", PasswordStrength.STRONG)
         }
 
         @DisplayName("길이만 8글자 미만이고 나머지 충족하는 테스트")
         @Test
         fun `When meet all criteria except for less than 8 characters expect normal`() {
-            val meter = PasswordStrengthMeter()
-            val result: PasswordStrength = meter.meter("a!123A4")
-            assertEquals(PasswordStrength.NORMAL, result)
+            assertStrength("a!123A4", PasswordStrength.NORMAL)
 
         }
 
         @DisplayName("숫자를 포함하지 않고 나머지 충족하는 테스트")
         @Test
         fun `When meet all criteria except for number expect normal`() {
+            assertStrength("!@ah!gf#", PasswordStrength.NORMAL)
+        }
+
+        private fun assertStrength(password: String, expectedString: PasswordStrength) {
             val meter = PasswordStrengthMeter()
-            val result: PasswordStrength = meter.meter("!@ah!gf#")
-            assertEquals(PasswordStrength.NORMAL, result)
+            val result: PasswordStrength = meter.meter(password)
+            assertEquals(expectedString, result)
         }
     }
 }
